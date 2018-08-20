a3f6156ca2ec472cec9f513d4496da7d4665deb3
Dylan <dylan1p@hotmail.com>
2018-08-20 23:01:40 +0100

    Include pagination for searching github users

--- api/user/user.controller.js
+++ api/user/user.controller.js
@@ -1,26 +1,60 @@
 const rp = require("request-promise");
+const parseLinkHeader = require("parse-link-header");
+const querystring = require("querystring");
 const User = require("./user.model");
 
 const GITHUB_API_URL = "https://api.github.com";
 
-const prepareQueryString = ({ name, location, language }) => {
+const prepareQueryString = ({ name, location, language, perPage, page}) => {
   let queryString = "";
 
   if (name) {
     queryString += name;
-  }
-  if (location) {
+  }  if (location) {
     queryString += `+location:${location}`;
-  }
-  if (language) {
+  }  if (language) {
     queryString += `+language:${language}`;
+  } if (perPage) {
+    queryString += `&per_page=${perPage}`;
+  } if (page) {
+    queryString += `&page=${page}`;
   }
 
   return queryString;
 };
 
+const preparePaginationLinks = (
+  host,
+  queryParams,
+  { next, prev, first, last }
+) => {
+
+  Object.keys(queryParams).forEach(
+    key => queryParams[key] === undefined || key === 'page' && delete queryParams[key]
+  );
+
+  const qs = querystring.stringify(queryParams);
+
+  const paginationLinks = {};
+
+  if (next) {
+    paginationLinks.next = `http://${host}/api/users?${qs}&page=${next.page}`;
+  }
+  if (prev) {
+    paginationLinks.prev = `http://${host}/api/users?${qs}&page=${prev.page}`;
+  }
+  if (first) {
+    paginationLinks.first = `http://${host}/api/users?${qs}&page=${first.page}`;
+  }
+  if (last) {
+    paginationLinks.last = `http://${host}/api/users?${qs}&page=${last.page}`;
+  }
+
+  return paginationLinks;
+};
+
 /* eslint-disable camelcase */
-const updateUser = (userId, user) => {
+const updateUser = (userId, user) =>
   User.findByIdAndUpdate(userId, user, { new: true })
     .then(({ id, login, avatar_url, url, score }) => ({
       id,
@@ -32,7 +66,6 @@ const updateUser = (userId, user) => {
     .catch(err => {
       throw new Error(`Error updating user : ${err}`);
     });
-};
 
 const saveUser = user => {
   const newUser = new User(user);
@@ -72,24 +105,45 @@ const getUsersGithub = params => {
     headers: {
       "User-Agent": "Request-Promise"
     },
+    resolveWithFullResponse: true,
     json: true
   })
-    .then(res => res.items)
+    .then(({ body, headers }) => {
+      let paginationLinks;
+
+      if (headers.link) {
+        paginationLinks = parseLinkHeader(headers.link);
+      }
+
+      return { items: body.items, paginationLinks };
+    })
     .catch(err => {
       throw new Error(`Error fetching from github, ${err}`);
     });
 };
 
 const getUsers = async (req, res) => {
-  const { name, location, language } = req.query;
+  const { host } = req.headers;
+  const { name, location, language, perPage, page } = req.query;
 
   if (name === undefined && location === undefined && language === undefined) {
     return res.status(400).send("Must include search parameters");
   }
 
   try {
-    const githubUsers = await getUsersGithub({ location, name, language });
-    const users = await saveUsers(githubUsers);
+    const { items, paginationLinks } = await getUsersGithub({
+      name,
+      location,
+      language,
+      perPage,
+      page
+    });
+
+    const users = await saveUsers(items);
+
+    if (paginationLinks) {
+      res.links(preparePaginationLinks(host, req.query, paginationLinks));
+    }
 
     return res.json(users);
   } catch (err) {
